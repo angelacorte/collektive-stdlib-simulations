3862e3507dd033d88c468f6acce8591501267718
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-11-04 17:30:14 +0100

    feat: add common entrypoint file

---
+++ src/main/kotlin/it/unibo/collektive/examples/gossip/Entrypoints.kt
@@ -0,0 +1,34 @@
+package it.unibo.collektive.examples.gossip
+
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
+import it.unibo.collektive.alchemist.device.sensors.RandomGenerator
+import it.unibo.collektive.alchemist.device.sensors.TimeSensor
+
+context(TimeSensor, EnvironmentVariables)
+fun Aggregate<Int>.isHappeningGossipEntrypoint() = isHappeningGossip {
+    (getTimeAsDouble() in 20.0..40.0) && (localId % 2 == 0)
+}
+
+context(EnvironmentVariables, TimeSensor, RandomGenerator)
+fun Aggregate<Int>.gossipEntrypoint() = gossipMax(
+    randomFromTimeElapsed()
+        .also { set("local-value", it) }
+) { first, second ->
+    second.compareTo(first)
+}.also { set("best-value", it) }
+
+context(EnvironmentVariables, TimeSensor, RandomGenerator)
+fun Aggregate<Int>.secondGossipEntrypoint() = secondGossip(
+    randomFromTimeElapsed()
+        .also { set("local-value", it) }
+) { first, second ->
+    second.compareTo(first)
+}.also { set("best-value", it) }
+
+context(EnvironmentVariables, TimeSensor, RandomGenerator)
+fun Aggregate<Int>.firstGossipEntrypoint() = firstGossip(
+    randomFromTimeElapsed().also { set("local-value", it) } )
+{ first, second ->
+    first <= second
+}.also { set("best-value", it) }

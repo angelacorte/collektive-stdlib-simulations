a1c9523098700b00ce9ec241f4c93fa3ed523bcc
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-11-04 19:59:14 +0100

    chore: silent alchemist warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,6 +128,10 @@ File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
             description = "Launches batch experiments for $capitalizedName"
             maxHeapSize = "${minOf(heap.toInt(), Runtime.getRuntime().availableProcessors() * taskSize)}m"
             File("data").mkdirs()
+            args(
+                "--verbosity",
+                "error"
+            )
         }
         runAllBatch.dependsOn(batch)
     }

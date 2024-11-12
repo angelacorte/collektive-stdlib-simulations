38fbae8f53ac746ef0161434a2e25a203608339d
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-11-12 18:20:46 +0100

    feat: use stdlib gossip

--- src/main/kotlin/it/unibo/collektive/examples/gossip/Entrypoints.kt
+++ src/main/kotlin/it/unibo/collektive/examples/gossip/Entrypoints.kt
@@ -5,53 +5,70 @@ import it.unibo.collektive.alchemist.device.sensors.AbsoluteTime
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.alchemist.device.sensors.RandomGenerator
 import it.unibo.collektive.alchemist.device.sensors.TimeSensor
+import it.unibo.collektive.examples.timeReplication.sharedTimer
+import it.unibo.collektive.examples.timeReplication.timeReplicated
 import it.unibo.collektive.stdlib.NonSelfStabilizingGossip.nonSelfStabilizingGossip
 import it.unibo.collektive.stdlib.SelfStabilizingGossip.gossipMax
 import it.unibo.collektive.stdlib.SelfStabilizingGossip.isHappeningAnywhere
-import java.time.Duration
-import kotlin.time.DurationUnit.SECONDS
-import kotlin.time.toKotlinDuration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 context(TimeSensor, EnvironmentVariables)
 fun Aggregate<Int>.isHappeningGossipEntrypoint() = isHappeningAnywhere {
     (getTimeAsDouble() % 100 < 50) && (localId % 2 == 0)
 }
 
-context(AbsoluteTime)
-fun Aggregate<Int>.sharingTime() = sharedTimer(5.0, getDeltaTime().toDouble(SECONDS))
+fun Aggregate<Int>.sharingTime(absoluteTime: AbsoluteTime) =
+    sharedTimer(5.toDuration(DurationUnit.SECONDS), absoluteTime.getDeltaTime())
 
-context(TimeSensor, EnvironmentVariables, RandomGenerator)
-fun Aggregate<Int>.timeReplication() = timeReplicated(
-    timeSensor = this@TimeSensor,
+//context(AbsoluteTime, EnvironmentVariables, RandomGenerator, TimeSensor)
+fun Aggregate<Int>.timeReplication(
+    absoluteTime: AbsoluteTime,
+    env: EnvironmentVariables,
+    randomGenerator: RandomGenerator,
+    timeSensor: TimeSensor,
+) = timeReplicated(
+    timeSensor = absoluteTime,
     process = {
       nonSelfStabilizingGossip(
-          randomFromTimeElapsed().also { set("local-value", it) }
+          randomFromTimeElapsed(timeSensor, randomGenerator).also { env["local-value"] = it }
       ) { first, second -> if (first <= second) first else second }
     },
     default = 42,
-    timeToLive = Duration.ofSeconds(1L).toKotlinDuration(),
+    timeToLive = 5.toDuration(DurationUnit.SECONDS),
     maxReplicas = 7,
 )
 
-context(EnvironmentVariables, TimeSensor, RandomGenerator)
-fun Aggregate<Int>.gossipEntrypoint() = gossipMax(
-    randomFromTimeElapsed()
-        .also { set("local-value", it) }
+fun Aggregate<Int>.gossipEntrypoint(
+    env: EnvironmentVariables,
+    randomGenerator: RandomGenerator,
+    timeSensor: TimeSensor,
+) = gossipMax(
+    randomFromTimeElapsed(timeSensor, randomGenerator)
+        .also { env["local-value"] = it }
 ) { first, second ->
     second.compareTo(first)
-}.also { set("best-value", it) }
+}.also { env["best-value"] = it }
 
-context(EnvironmentVariables, TimeSensor, RandomGenerator)
-fun Aggregate<Int>.secondGossipEntrypoint() = secondGossip(
-    randomFromTimeElapsed()
-        .also { set("local-value", it) }
+fun Aggregate<Int>.secondGossipEntrypoint(
+    env: EnvironmentVariables,
+    randomGenerator: RandomGenerator,
+    timeSensor: TimeSensor,
+) = secondGossip(
+    env,
+    randomFromTimeElapsed(timeSensor, randomGenerator)
+        .also { env["local-value"] = it }
 ) { first, second ->
     second.compareTo(first)
-}.also { set("best-value", it) }
+}.also { env["best-value"] = it }
 
-context(EnvironmentVariables, TimeSensor, RandomGenerator)
-fun Aggregate<Int>.firstGossipEntrypoint() = firstGossip(
-    randomFromTimeElapsed().also { set("local-value", it) } )
+fun Aggregate<Int>.firstGossipEntrypoint(
+    env: EnvironmentVariables,
+    randomGenerator: RandomGenerator,
+    timeSensor: TimeSensor,
+) = firstGossip(
+    env,
+    randomFromTimeElapsed(timeSensor, randomGenerator).also { env["local-value"] = it } )
 { first, second ->
     first <= second
-}.also { set("best-value", it) }
+}.also { env["best-value"] = it }

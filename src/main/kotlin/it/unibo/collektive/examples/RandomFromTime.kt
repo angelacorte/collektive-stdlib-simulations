31333df700450896f8644030913c5474b8304125
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2025-12-19 12:41:34 +0100

    refactor: change function name and take random range as parameter

--- src/main/kotlin/it/unibo/collektive/examples/RandomFromTime.kt
+++ src/main/kotlin/it/unibo/collektive/examples/RandomFromTime.kt
@@ -1,15 +1,20 @@
 package it.unibo.collektive.examples
 
+import it.unibo.alchemist.util.RandomGenerators.nextDouble
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.alchemist.device.sensors.RandomGenerator
 import it.unibo.collektive.alchemist.device.sensors.TimeSensor
+import org.apache.commons.math3.random.RandomGenerator
 
-fun Aggregate<Int>.random(randomGenerator: RandomGenerator) = evolve(randomGenerator.nextRandomDouble()) { it }
+fun Aggregate<Int>.keepRandom(
+    randomGenerator: RandomGenerator,
+    startInclusive: Double,
+    endInclusive: Double,
+) = evolve(randomGenerator.nextDouble(startInclusive, endInclusive)) { it }
 
 fun Aggregate<Int>.randomFromTimeElapsed(
     timeSensor: TimeSensor,
     randomGenerator: RandomGenerator,
 ) = when {
-    timeSensor.getTimeAsDouble() % 1000 <= 150.0 -> random(randomGenerator)
-    else -> random(randomGenerator)
+    timeSensor.getTimeAsDouble() <= 400.0 -> keepRandom(randomGenerator, -1.0, 1.0)
+    else -> keepRandom(randomGenerator, -0.5, 0.5)
 }

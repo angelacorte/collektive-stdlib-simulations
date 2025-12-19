e6da65d6cd49d27fd94777a754edd380ccf009b4
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2025-12-19 12:40:36 +0100

    docs: add doc

--- src/main/kotlin/it/unibo/alchemist/actions/MoveCluster.kt
+++ src/main/kotlin/it/unibo/alchemist/actions/MoveCluster.kt
@@ -8,6 +8,19 @@ import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.Reaction
 import it.unibo.alchemist.model.actions.AbstractAction
 
+/**
+ * An action that moves a node within an environment by modifying its position within a specified range.
+ *
+ * This class performs a movement operation on a given node, altering its position
+ * within the environment by adding the specified `movingRange` to the x-coordinate value,
+ * while maintaining the y-coordinate unchanged. The motion is constrained to the environment's bounds.
+ *
+ * @param T The type of the concentration present in the node.
+ * @param P The type of the position used in the environment.
+ * @property environment The environment in which the node operates.
+ * @property node The node being moved.
+ * @property movingRange The range of movement for the node along the x-axis.
+ */
 class MoveCluster<T : Any, P : Position<P>>(
     private val environment: Environment<T, P>,
     private val node: Node<T>,

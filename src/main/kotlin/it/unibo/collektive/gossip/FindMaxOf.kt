a5596bf29cea208430aaef3f9912cffab7ad3de3
Danilo Pianini <danilo.pianini@unibo.it>
2026-02-17 18:02:42 +0100

    style: improve code

--- src/main/kotlin/it/unibo/collektive/gossip/FindMaxOf.kt
+++ src/main/kotlin/it/unibo/collektive/gossip/FindMaxOf.kt
@@ -22,21 +22,21 @@ object FindMaxOf {
         val localGossip = GossipValue<ID, Value>(best = local)
         return share(localGossip) { gossip ->
             gossip.neighbors.values.fold(localGossip) { current, neighbor ->
-                val comparatorResult = comparator.compare(neighbor.best, current.best)
                 when {
                     localId in neighbor.path -> current // Ignore paths that loop back
-                    comparatorResult == 0 -> // If values tie, select based on path
-                        when {
-                            neighbor.path.size == current.path.size ->
-                                // If paths are of equal length, compare their last element
-                                // (they necessarily come from different neighbors)
-                                listOf(neighbor, current).minBy { it.path.last() }
-                            // Select the shortest path
-                            else -> listOf(neighbor, current).minBy { it.path.size }
-                        }
-                    // Pick the best value according to the comparator
-                    comparatorResult < 0 -> neighbor
-                    else -> current
+                    else -> when(comparator.compare(neighbor.best, current.best)) {
+                        0 -> when { // If values tie, select based on path
+                                neighbor.path.size == current.path.size ->
+                                    // If paths length tie, compare their last element
+                                    // (they necessarily come from different neighbors)
+                                    listOf(neighbor, current).minBy { it.path.last() }
+                                // Select the shortest path
+                                else -> listOf(neighbor, current).minBy { it.path.size }
+                            }
+                        // Pick the best value according to the comparator
+                        in 1..Int.MAX_VALUE -> current
+                        else -> current
+                    }
                 }
             }.addHop(localId)
         }.best
